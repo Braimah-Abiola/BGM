3fc5d6d43bbbe54fcaaf33c23cba39d60e5e984e
braimahabiola <braimahabiola.official@gmail.com>
2020-11-15 11:52:27 +0000

    first commit

---
+++ ReadMe.txt
@@ -0,0 +1,44 @@
+design all images needed for the website
+How to link website to social media page
+Change the icon on the title bar and the names for each page
+Take images of the team members
+Get the youtube links for the Games
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+COMPLETED TASKS
+Added a register form or page
+Finally added the back to top button to all pages
+Added the login page
+Finally added a custom back to top button
+Added a search bar to the blog page
+Created the following icons:
+-home
+-contact us
+-videos
+-about
+-pc
+-consoles
+-news
+-games
+-reviews
+-login
+-register
+Finally added the Quotes section of the website
+Currently on the 3rd page under the quotes section
+Added images of game characters(png)
+-Kratos, Mario,
+Finally Added Videos section
---
+++ js/main.js
@@ -0,0 +1,220 @@
+/* =================================
+------------------------------------
+	EndGam - Gaming Magazine Template
+	Version: 1.0
+ ------------------------------------
+ ====================================*/
+
+
+'use strict';
+
+
+$(window).on('load', function() {
+	/*------------------
+		Preloder
+	--------------------*/
+	$(".loader").fadeOut();
+	$("#preloder").delay(400).fadeOut("slow");
+
+});
+
+(function($) {
+	/*------------------
+		Navigation
+	--------------------*/
+	$('.primary-menu').slicknav({
+		appendTo:'.header-warp',
+		closedSymbol: '<i class="fa fa-angle-down"></i>',
+		openedSymbol: '<i class="fa fa-angle-up"></i>'
+	});
+
+
+// Search model
+	$('.search-switch').on('click', function() {
+		$('.search-model').fadeIn(400);
+	});
+
+	$('.search-close-switch').on('click', function() {
+		$('.search-model').fadeOut(400,function(){
+			$('#search-input').val('');
+		});
+	});
+
+
+	/*------------------
+		Background Set
+	--------------------*/
+	$('.set-bg').each(function() {
+		var bg = $(this).data('setbg');
+		$(this).css('background-image', 'url(' + bg + ')');
+	});
+
+
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
+
+
+	/*------------------
+		Hero Slider
+	--------------------*/
+	$('.hero-slider').owlCarousel({
+		loop: true,
+		nav: true,
+		dots: true,
+		navText: ['', '<img src="img/icons/solid-right-arrow.png">'],
+		mouseDrag: false,
+		animateOut: 'fadeOut',
+		animateIn: 'fadeIn',
+		items: 1,
+		//autoplay: true,
+		autoplayTimeout: 10000,
+	});
+
+	var dot = $('.hero-slider .owl-dot');
+	dot.each(function() {
+		var index = $(this).index() + 1;
+		if(index < 10){
+			$(this).html('0').append(index + '.');
+		}else{
+			$(this).html(index + '.');
+		}
+	});
+
+
+
+
+
+
+	/*------------------
+		Video Popup
+	--------------------*/
+	$('.video-popup').magnificPopup({
+  		type: 'iframe'
+	});
+
+	$('#stickySidebar').stickySidebar({
+	    topSpacing: 60,
+	    bottomSpacing: 60
+	});
+///////////////////////////
+	// On Scroll
+	$(window).on('scroll', function() {
+		var wScroll = $(this).scrollTop();
+
+		// Fixed nav
+		wScroll > 1 ? $('#nav').addClass('fixed-nav') : $('#nav').removeClass('fixed-nav');
+
+		// Back To Top Appear
+		wScroll > 700 ? $('#back-to-top').fadeIn() : $('#back-to-top').fadeOut();
+	});
+
+
+	///////////////////////////
+	// Scrollspy
+	$('body').scrollspy({
+		target: '#nav',
+		offset: $(window).height() / 2
+	});
+
+	///////////////////////////
+	// Smooth scroll
+	$("#nav .main-nav a[href^='#']").on('click', function(e) {
+		e.preventDefault();
+		var hash = this.hash;
+		$('html, body').animate({
+			scrollTop: $(this.hash).offset().top
+		}, 600);
+	});
+
+	$('#back-to-top').on('click', function(){
+		$('body,html').animate({
+			scrollTop: 0
+		}, 600);
+	});
+
+
+})(jQuery);
+
+
+(function ($) {
+    "use strict";
+
+
+     /*==================================================================
+    [ Focus input ]*/
+    $('.input100').each(function(){
+        $(this).on('blur', function(){
+            if($(this).val().trim() != "") {
+                $(this).addClass('has-val');
+            }
+            else {
+                $(this).removeClass('has-val');
+            }
+        })    
+    })
+  
+  
+    /*==================================================================
+    [ Validate ]*/
+    var input = $('.validate-input .input100');
+
+    $('.validate-form').on('submit',function(){
+        var check = true;
+
+        for(var i=0; i<input.length; i++) {
+            if(validate(input[i]) == false){
+                showValidate(input[i]);
+                check=false;
+            }
+        }
+
+        return check;
+    });
+
+
+    $('.validate-form .input100').each(function(){
+        $(this).focus(function(){
+           hideValidate(this);
+        });
+    });
+
+    function validate (input) {
+        if($(input).attr('type') == 'email' || $(input).attr('name') == 'email') {
+            if($(input).val().trim().match(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/) == null) {
+                return false;
+            }
+        }
+        else {
+            if($(input).val().trim() == ''){
+                return false;
+            }
+        }
+    }
+
+    function showValidate(input) {
+        var thisAlert = $(input).parent();
+
+        $(thisAlert).addClass('alert-validate');
+    }
+
+    function hideValidate(input) {
+        var thisAlert = $(input).parent();
+
+        $(thisAlert).removeClass('alert-validate');
+    }
+    
+    
+
+})(jQuery);
